f8f85d980071fdb56233e01b2c1795941c1f350b
Manav2031 <92452757+Manav2031@users.noreply.github.com>
2023-03-24 10:04:34 +0000

    updated meddata

--- src/components/meddata.jsx
+++ src/components/meddata.jsx
@@ -3,12 +3,13 @@ import React from "react"
 export default function MedData(props) {
     return (
         <div>
-            <p> {props.name} </p>
-            <p> {props.description} </p>
-            <p> {props.expirydate} </p>
-            <p> {props.quantity} </p>
-            <p> {props.purchasedate} </p>
-            <p> {props.price} </p>
+            <p> Name: {props.name} </p>
+            <p> Description: {props.description} </p>
+            <p> Expiry Date: {props.expirydate} </p>
+            <p> Quantity: {props.quantity} </p>
+            <p> Purchase Date: {props.purchasedate} </p>
+            <p> Price: {props.price} </p>
+            <hr />
         </div>
     )
 }
