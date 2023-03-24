f0c429caed253f7c8aea8540de940bca97d39b30
Manav2031 <92452757+Manav2031@users.noreply.github.com>
2023-03-24 15:10:25 +0530

    meddashboard added

--- src/components/meddata.jsx
+++ src/components/meddata.jsx
@@ -1,1 +1,14 @@
-import React from "react";
+import React from "react"
+
+export default function MedData(props) {
+    return (
+        <div>
+            <p> {props.name} </p>
+            <p> {props.description} </p>
+            <p> {props.expirydate} </p>
+            <p> {props.quantity} </p>
+            <p> {props.purchasedate} </p>
+            <p> {props.price} </p>
+        </div>
+    )
+}
