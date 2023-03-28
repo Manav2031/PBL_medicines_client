379eec26f49dab076fa51db837115088dca9904c
Manav2031 <92452757+Manav2031@users.noreply.github.com>
2023-03-28 09:51:05 +0000

    updated css

--- src/components/meddata.jsx
+++ src/components/meddata.jsx
@@ -2,14 +2,13 @@ import React from "react"
 
 export default function MedData(props) {
     return (
-        <div>
-            <p> Name: {props.name} </p>
-            <p> Description: {props.description} </p>
-            <p> Expiry Date: {props.expirydate} </p>
-            <p> Quantity: {props.quantity} </p>
-            <p> Purchase Date: {props.purchasedate} </p>
-            <p> Price: {props.price} </p>
-            <hr />
+        <div className="contact-card">
+            <h3> Name: <div className="med-color">{props.name}</div> </h3>
+            <p> <b>Description:</b> {props.description} </p>
+            <p> <b>Expiry Date:</b> {props.expirydate} </p>
+            <p> <b>Quantity:</b> {props.quantity} </p>
+            <p> <b>Purchase Date:</b> {props.purchasedate} </p>
+            <p> <b>Price:</b> {props.price} </p>
         </div>
     )
 }
