1e8e20b4bb97685c8252f55162435b7bdf80389a
Varad Pundlik <93179552+varadpundlik@users.noreply.github.com>
2023-03-31 10:20:18 +0000

    added addmdpage

--- src/components/addNewMed.jsx
+++ src/components/addNewMed.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 
 export default function AddMed(){
-    const {med}=useState({
+    const {med,setMed}=useState({
         name:" ",
-        quantitY:" ",
-
+        quantity:0,
+        expiery_date:" ",
+        price:0
     })
 
     return(
