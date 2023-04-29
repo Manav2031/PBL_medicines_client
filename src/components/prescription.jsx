91b576a963163cc0ee6af020864fba739b99a26b
Manav2031 <92452757+Manav2031@users.noreply.github.com>
2023-04-29 15:21:47 +0000

    Pending changes exported from your codespace

--- src/components/prescription.jsx
+++ src/components/prescription.jsx
@@ -1,21 +1,94 @@
 import React,{useState} from "react";
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import Details from "../components/details";
-import MedDetails from "../components/meddetails";
 
 export default function Prescription() {
+    const [details,setDetails] = useState({
+        name:"",
+        doctor:"",
+        email:""
+    })
+
+    const [medicine,setMedicine] = useState({
+        name:"",
+        frequency:0,
+        days:0,
+    })
+
+
+    function handleChange1(event) {
+        const {name,value}=event.target;
+        setDetails(prevValue => {
+            return {
+                ...prevValue,
+                [name]: value
+            }
+        })
+    }
+
+    function handleChange2(event) {
+        const {name,value}=event.target;
+        setMedicine(prevValue => {
+            return {
+                ...prevValue,
+                [name]: value
+            }
+        })
+    }
 
     function handleSubmit(event) {
         event.preventDefault()
+        console.log(details)
+        console.log(medicine)
     }
 
     return (
-        <div>
-      <Details />
-      <MedDetails />
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        <Form className="lovw">
+        <Form.Group className="mb-3">
+          <Form.Label>Name of Patient</Form.Label>
+          <Form.Control type="text" placeholder="Enter Patient's Name" className="tish"  name="name"
+                  value={details.name}
+                  onChange={handleChange1}/>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Name of Doctor</Form.Label>
+          <Form.Control type="text" placeholder="Enter Doctor's Name" className="tish"  name="doctor"
+                  value={details.frequency}
+                  onChange={handleChange1}/>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Patient's Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter Patient's Email ID" className="tish"  name="email"
+                  value={details.email}
+                  onChange={handleChange1}/>
+        </Form.Group>
+        <Form.Group className="mb-3">
+        <Form.Label>Name of Medicine</Form.Label>
+        <Form.Control type="text" placeholder="Enter Name" className="tish"  name="name"
+                value={medicine.name}
+                onChange={handleChange2}/>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Number of times in a day</Form.Label>
+        <Form.Control type="text" placeholder="Enter Frequency" className="tish"  name="frequency"
+                value={medicine.frequency}
+                onChange={handleChange2}/>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Number of days</Form.Label>
+        <Form.Control type="text" placeholder="Enter Number of Days" className="tish"  name="days"
+                value={medicine.days}
+                onChange={handleChange2}/>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Quantity</Form.Label>
+        <Form.Control type="text" placeholder="Quantity" className="tish"
+                value={medicine.frequency * medicine.days}
+                onChange={handleChange2}/>
+      </Form.Group>
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
         Submit
       </Button>
-      </div> 
+      </Form>
     )
 }
