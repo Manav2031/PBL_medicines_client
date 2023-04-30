9ba138b7ddb6d18bc045e039cf7bbe369c945717
Manav2031 <manavmehta2031@gmail.com>
2023-05-01 00:23:21 +0530

    updated quantity in prescription

--- src/components/prescription.jsx
+++ src/components/prescription.jsx
@@ -11,9 +11,11 @@ export default function Prescription(props) {
 
     const [medicine,setMedicine] = useState({
         name:"",
-        frequency:0,
-        days:0
+        frequency:"",
+        days:"",
+        quantity:""
     })
+
 
 
     function handleChange1(event) {
@@ -28,6 +30,18 @@ export default function Prescription(props) {
 
     function handleChange2(event) {
         const {name,value}=event.target;
+        if(name==="frequency" || name==="days")
+        {
+            const newQuantity=parseInt(medicine.frequency)*parseInt(medicine.days);
+            setMedicine(prevValue => {
+                return {
+                    ...prevValue,
+                    [name]: value,
+                    quantity: newQuantity
+                }
+            })
+        }
+        else {
         setMedicine(prevValue => {
             return {
                 ...prevValue,
@@ -35,14 +49,16 @@ export default function Prescription(props) {
             }
         })
     }
+}
 
     function handleSubmit(event) {
         props.onAdd(medicine);
         setMedicine({
             name: "",
-            frequency:0,
-            days:0
+            frequency:"",
+            days:""
         })
+        console.log(medicine)
         event.preventDefault();
     }
 
@@ -82,12 +98,6 @@ export default function Prescription(props) {
         <Form.Label>Number of days</Form.Label>
         <Form.Control type="text" placeholder="Enter Number of Days" className="tish"  name="days"
                 value={medicine.days}
-                onChange={handleChange2}/>
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Quantity</Form.Label>
-        <Form.Control type="text" placeholder="Quantity" className="tish"
-                value={medicine.frequency * medicine.days}
                 onChange={handleChange2}/>
       </Form.Group>
         <Button variant="primary" type="submit" onClick={handleSubmit}>
