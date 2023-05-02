2e284379fd302929c14a5c8871f678bb4598dd62
Manav2031 <92452757+Manav2031@users.noreply.github.com>
2023-05-02 10:08:28 +0000

    updated prescription design

--- src/components/displayprescription.jsx
+++ src/components/displayprescription.jsx
@@ -2,12 +2,10 @@ import React from "react";
 
 export default function Display(props) {
     return (
-        <div className="display-note">
-        <div className="note">
-            <h1> {props.name} </h1>
-            <p> {props.frequency} </p>
-            <p> {props.days} </p>
-        </div>
+        <div className="prescription-form">
+            <h1> Name: {props.name} </h1>
+            <h3> Number of Times: {props.frequency} </h3>
+            <h3> Number of Days: {props.days} </h3>
         </div>
     )
 }
--- src/components/prescDetails.jsx
+++ src/components/prescDetails.jsx
@@ -1,6 +1,6 @@
 export default function PrecDetail(props){
     return(
-    <div>
+    <div className="prescription-form">
         <h1 align="centre">Prescription</h1>
         <h2 align="centre">Patient's Name : {props.name}</h2>
         <h2 align="centre">Doctor's Name : {props.doctor}</h2>
