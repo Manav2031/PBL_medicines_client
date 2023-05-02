2140fce531ef9bc34bdcec421ebf3b910b7d07a5
Disha Saxena <116488034+disha1234t4rdkcbvdfkbf@users.noreply.github.com>
2023-05-02 09:48:31 +0000

    home page

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,8 +2,44 @@ import React from "react";
 
 export default function Home() {
     return (
-        <div>
-            <h1> Home </h1>
+        // <div>
+        //     <h1> Home </h1>
+        // </div>
+        <main>
+        <div class="intro">
+          <h1>THE MED PORTAL</h1>
+          <p>A medicine supply chain optimization project.</p>
+          {/* <button>Learn More</button> */}
         </div>
+        <div class="achievements">
+          <div class="work">
+          <h2>ABOUT THE PROJECT</h2>
+            <i class="fas fa-atom"></i>
+            
+            <p class="work-heading">Projects</p>
+            <p class="work-text">I have worked on many projects and I am very proud of them. I am a very good developer and I am always looking for new projects.</p>
+          </div>
+          <div class="work">
+            <i class="fas fa-skiing"></i>
+            <p class="work-heading">Skills</p>
+            <p class="work-text">I have a lot of skills and I am very good at them. I am very good at programming and I am always looking for new skills.</p>
+          </div>
+          <div class="work">
+            <i class="fas fa-ethernet"></i>
+            <p class="work-heading">Network</p>
+            <p class="work-text">I have a lot of network skills and I am very good at them. I am very good at networking and I am always looking for new network skills.</p>
+          </div>
+        </div>
+        <div class="about-me">
+          <div class="about-me-text">
+            <h2>About Me</h2>
+            <p>I am a web developer and I love to create websites. I am a very good developer and I am always looking for new projects. I am a very good developer and I am always looking for new projects.</p>
+          </div>
+          <img
+          src="https://images.unsplash.com/photo-1596495578065-6e0763fa1178?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" alt="me">
+          </img>
+        </div>
+      </main>
+     
     )
 }
