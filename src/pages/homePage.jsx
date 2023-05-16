81ea0216b1b313e88096378defdf88582227b719
divyank007 <divyanksagvekar37@gmail.com>
2023-05-16 14:18:06 +0530

    updated homepage

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -14,7 +14,7 @@ export default function Home() {
         // </div>
         <main>
         <div class="intro">
-          <h1>THE <span className="ghj">MED</span> PORTAL</h1>
+          <h1>THE MED PORTAL</h1>
           <p>" A medicine supply chain optimization<br></br> <span class="gh">project  "</span></p>
           <button onClick={handleClick}>GET STARTED</button>
         
@@ -83,15 +83,85 @@ export default function Home() {
             <p class="work-text">Verify authenticity of drugs using blockchain</p>
           </div>
         </div> */}
-        <div class="about-me">
+        {/* <div class="about-me">
           <div class="about-me-text">
             <h2>ABOUT US</h2>
-            {/* <p></p> */}
+            
           </div>
-          {/* <img
-          src="https://images.unsplash.com/photo-1596495578065-6e0763fa1178?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" alt="me">
-          </img> */}
+        
+        </div> */}
+        <div class="container text-center mt-5 mb-2">
+        <h3 class="mb-0">MEET THE TEAM</h3></div>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="bg-white p-3 text-center rounded box"><img class="img-responsive rounded-circle" src="https://i.imgur.com/uppKNuF.jpg" width="90"></img>
+                    <h5 class="mt-3 name">Samuel Plamer</h5><span class="work d-block">Comapay agents house</span><span class="work d-block">real estate</span>
+                    <div class="mt-4 about"><span>is a long established fact that eader&nbsp; will be distracted by the readable content.</span></div>
+                    <div class="mt-4">
+                    <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="bg-white p-3 text-center rounded box"><img class="img-responsive rounded-circle" src="https://i.imgur.com/oJmLthK.jpg" width="90"></img>
+                    <h5 class="mt-3 name">Nancy Markus</h5><span class="work d-block">Comapay agents house</span><span class="work d-block">real estate</span>
+                    <div class="mt-4 about"><span>is a long established fact that eader&nbsp; will be distracted by the readable content.</span></div>
+                    <div class="mt-4">
+                    <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="bg-white p-3 text-center rounded box"><img class="img-responsive rounded-circle" src="https://i.imgur.com/oJmLthK.jpg" width="90"></img>
+                    <h5 class="mt-3 name">Nancy Markus</h5><span class="work d-block">Comapay agents house</span><span class="work d-block">real estate</span>
+                    <div class="mt-4 about"><span>is a long established fact that eader&nbsp; will be distracted by the readable content.</span></div>
+                    <div class="mt-4">
+                    <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="bg-white p-3 text-center rounded box"><img class="img-responsive rounded-circle" src="https://i.imgur.com/HFpxxJz.jpg" width="90"></img>
+                    <h5 class="mt-3 name">Martha Col</h5><span class="work d-block">Comapay agents house</span><span class="work d-block">real estate</span>
+                    <div class="mt-4 about"><span>is a long established fact that eader&nbsp; will be distracted by the readable content.</span></div>
+                    <div class="mt-4">
+                    <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="bg-white p-3 text-center rounded box"><img class="img-responsive rounded-circle" src="https://i.imgur.com/MZm1LNz.jpg" width="90"></img>
+                    <h5 class="mt-3 name">Marry Jones</h5><span class="work d-block">Comapay agents house</span><span class="work d-block">real estate</span>
+                    <div class="mt-4 about"><span>is a long established fact that eader&nbsp; will be distracted by the readable content.</span></div>
+                    <div class="mt-4">
+                        {/* <h6 class="v-profile">View Profile</h6> */}
+                        <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
       </main>
      
     )
